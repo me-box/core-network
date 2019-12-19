f5c421e95b437d79c100a2dcb36cacafc5bd02f4
Cjen1 <cjj39@cam.ac.uk>
2019-12-19 15:16:04 +0000

    lib/env.ml update deps, clean up logic

--- lib/env.ml
+++ lib/env.ml
@@ -2,20 +2,20 @@ let secrets_dir = Fpath.v "/run/secrets/"
 
 let cm_key = ref ""
 
+let on_error f = function Error msg -> f msg ; Error msg | v -> v
+
 let cm_key () =
+  let ( let* ) = Rresult.R.bind in
   if !cm_key <> "" then Rresult.R.ok !cm_key
   else
-    let key_file = Fpath.add_seg secrets_dir "DATABOX_NETWORK_KEY" in
-    let get_key file = Base64.encode (String.trim file) in
-    Rresult.R.map get_key (Bos.OS.File.read key_file)
-    |> function
-    | Ok key ->
-        cm_key := key ;
-        Rresult.R.ok key
-    | Error msg ->
-        Logs.err (fun m ->
-            m "[env] DATABOX_NETWORK_KEY %a" Rresult.R.pp_msg msg) ;
-        Error msg
+    (let key_file = Fpath.add_seg secrets_dir "DATABOX_NETWORK_KEY" in
+     let* file = Bos.OS.File.read key_file in
+     let* key = file |> String.trim |> Base64.encode in
+     cm_key := key ;
+     Rresult.R.ok key)
+    |> on_error (fun msg ->
+           Logs.err (fun m ->
+               m "[env] DATABOX_NETWORK_KEY %a" Rresult.R.pp_msg msg))
 
 let https_creds () =
   let cert_file = Fpath.add_seg secrets_dir "DATABOX_NETWORK.pem" in
